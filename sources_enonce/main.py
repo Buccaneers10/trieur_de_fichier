bdb7614447c325947361ade561fd455df920cec0
Buccaneers10 <boucanier.pro@protonmail.com>
2022-09-27 19:12:48 +0200

    Récupération du chemin de dossier dans lequel se trouve les fichiers

--- sources_enonce/main.py
+++ sources_enonce/main.py
@@ -6,3 +6,12 @@ bmp, png, jpg : Images
 txt, pptx, csv, xls, odp, pages : Documents
 autres : Divers
 """
+# Les étapes
+
+# Associer les extensions aux dossiers cibles 
+
+# Récupérer le chemin du dossier dans lequel se trouve les fichiers
+# Récupérer les fichiers à l'intérieur du dossier
+# Trier les fichiers selon leur extension
+
+BASE_DIR = '/home/buccaneers/Bureau/Buccaneers/Python/trieur_de_fichier/sources_enonce/data'
